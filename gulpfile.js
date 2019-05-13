95857b641ef7410946a680b6050af1cdebb3986c
Julien Martel <julienbmartel@gmail.com>
2019-05-13 11:19:10 -0500

    integrated clean-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 // const browserSync = require('browser-sync').create()
 // const sass = require('gulp-sass');
 // const concat = require('gulp-concat');
-// const cleanCSS = require('gulp-clean-css');
 // const imagemin = require('gulp-imagemin');
 // const babel = require('gulp-babel');
 // const uglify = require('gulp-uglify');
@@ -14,9 +14,10 @@ const autoprefixer = require('gulp-autoprefixer');
 gulp.task("styles", () => 
   gulp.src('src/css/**/*.css')
     .pipe(autoprefixer())
+    .pipe(cleanCSS())
+
     // .pipe(sass().on('error', sass.logError))
     // .pipe(concat('styles.css'))
-    // .pipe(cleanCSS())
     // .pipe(gulp.dest('dest/css/'))
     // .pipe(browserSync.stream())
 )
