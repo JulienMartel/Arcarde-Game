fd3f6279d0e90cf6c62b6a61777009e06845f919
Julien Martel <julienbmartel@gmail.com>
2019-05-13 11:37:57 -0500

    mend

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,12 @@ gulp.task("js", () =>
 )
 
 gulp.task("html", () => 
-  gulp.src('index.html')
+  gulp.src('src/index.html')
     .pipe(gulp.dest('dest/'))
 )
+
+gulp.task("all", gulp.series("styles", "img", "js", "html", () => {
+  gulp.watch('src/css/**/*.css', gulp.series('styles'))
+  gulp.watch('src/js/**/*.js', gulp.series('js'))
+  gulp.watch('src/index.html', gulp.series('html'))
+}))
