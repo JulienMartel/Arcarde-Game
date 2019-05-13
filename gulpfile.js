636ce2d50e979610ad8c09959fa1f0965fcc9267
Julien Martel <julienbmartel@gmail.com>
2019-05-13 11:19:58 -0500

    integrated image-min

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,11 @@
 const gulp = require('gulp')
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 // const browserSync = require('browser-sync').create()
 // const sass = require('gulp-sass');
 // const concat = require('gulp-concat');
-// const imagemin = require('gulp-imagemin');
-// const babel = require('gulp-babel');
 // const uglify = require('gulp-uglify');
 // const rename = require("gulp-rename");
 
@@ -20,4 +19,10 @@ gulp.task("styles", () =>
     // .pipe(concat('styles.css'))
     // .pipe(gulp.dest('dest/css/'))
     // .pipe(browserSync.stream())
+)
+
+gulp.task("img", () => 
+  gulp.src('src/images/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dest/images'))
 )
