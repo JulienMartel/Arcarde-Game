2369e816f415f88363a4729e836ae1835192fbf9
Julien Martel <julienbmartel@gmail.com>
2019-05-13 11:30:50 -0500

    integrated uglify to minify js

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,10 @@ const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
 const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
 
 // const browserSync = require('browser-sync').create()
 // const sass = require('gulp-sass');
-// const uglify = require('gulp-uglify');
 // const rename = require("gulp-rename");
 
 
@@ -32,8 +32,9 @@ gulp.task("js", () =>
   gulp.src('src/js/**/*.js' ['src/js/resources.js', 'src/js/app.js', 'src/js/engine.js'])
     .pipe(babel())
     .pipe(concat('main.js'))
+    .pipe(uglify())
     // .pipe(gulp.dest('dest/js'))
-    // .pipe(uglify())
+    
     // .pipe(rename("js/all.min.js"))
     // .pipe(gulp.dest('dest'))
     // .pipe(browserSync.stream())
